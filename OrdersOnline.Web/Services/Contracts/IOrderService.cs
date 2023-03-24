deb622a1460a5c7db19a5dd377b7bead363db8ab
Spark <piotr.rac@gmail.com>
2023-03-24 15:52:04 +0100

    Api data dbContext changes

---
+++ OrdersOnline.Web/Services/Contracts/IOrderService.cs
@@ -0,0 +1,9 @@
+﻿using OrdersOnline.Models.Dto;
+
+namespace OrdersOnline.Web.Services.Contracts
+{
+    public interface IOrderService
+    {
+        Task<IEnumerable<OrderDTO>> GetItems();
+    }
+}
